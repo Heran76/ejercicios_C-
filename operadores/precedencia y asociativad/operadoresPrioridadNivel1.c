3918828efdb3ad7a7e8b1a351b394e50a4ec1e26
Heran76 <heran76@gmail.com>
2024-09-21 20:27:51 +0200

    prioridad nivel 1

---
+++ operadores/precedencia y asociativad/operadoresPrioridadNivel1.c
@@ -0,0 +1,16 @@
+//Operadadore de Prioridad Nivel 1 mayor prioridad.
+/*
+1 el parentesis es le de mayor prioridad.
+2 el corchete define array, matrices, vectores 
+3- -> el operador flecha se uliza acceso a extructura
+4 el punto es el ultimo el punto se utiliza para acceder de ves de puntero a extrura.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    a = (3-5)*7
+    return 0;
+}

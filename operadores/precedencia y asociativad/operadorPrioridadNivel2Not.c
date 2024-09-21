2988027f29155258b4556302f03ef74d03d1d59c
Heran76 <heran76@gmail.com>
2024-09-21 21:21:17 +0200

    not logico

---
+++ operadores/precedencia y asociativad/operadorPrioridadNivel2Not.c
@@ -0,0 +1,17 @@
+/*
+Operadores de prioridad nivel 2, Not logic y not a nivel de bit.
+(!) not logico
+~ para sacar la virgulilla alt + ñ
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int a, b, c;
+   a = 12;
+   printf("el valor not opuesto al logico ! es : %d",!a);
+ 
+    return 0;
+}

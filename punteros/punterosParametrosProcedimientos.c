81edbee279e458a21019fb4ae0ae08891d3dd393
Heran76 <heran76@gmail.com>
2024-09-22 13:21:46 +0200

    punteros como parametros

---
+++ punteros/punterosParametrosProcedimientos.c
@@ -0,0 +1,18 @@
+/*
+ Punteros como parámetros de entrada a procedimientos.
+*/
+#include <stdio.h>
+#include <stdio.h>
+
+void procd1(int *a) // aqui declaramos el parametro como puntero
+{
+    *a = *a * 100; // aqui damos los datos de la función como puntero.
+}
+
+int main()
+{
+     int a = 125;
+     procd1(&a); // para pasar el contenido de la funcion hay que adignarele la memoria como parámetro
+     printf("%d",a);
+     return 0;
+}

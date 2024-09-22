537bda0c9fe80342883259050ec6271b892d06bf
Heran76 <heran76@gmail.com>
2024-09-22 20:23:22 +0200

    punterapuntero

---
+++ punteros/punterosSobrePunteros.c
@@ -0,0 +1,18 @@
+/*
+Punteros sobre punteros
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+    int a = 58;
+    int *p1 = &a;
+    int **p2 = &p1;
+    *p1 = 67;
+    **p2 = 1987;
+    
+    printf("%d\n",a);
+    return 0;
+}

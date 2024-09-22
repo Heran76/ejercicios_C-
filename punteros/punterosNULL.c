f6cc977ef3f2ea1ea201f526d6d41148eeb0409a
Heran76 <heran76@gmail.com>
2024-09-22 20:02:20 +0200

    punteros

---
+++ punteros/punterosNULL.c
@@ -0,0 +1,16 @@
+/*
+ Punteros NUll
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    long long int B = 0;
+    printf("%d\n", sizeof NULL);
+    int a = 85;
+    int *p = NULL;
+    printf("%d\n", sizeof p);
+     
+    return 0;
+}

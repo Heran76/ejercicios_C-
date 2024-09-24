ac33548061750980bd5e97119e809d6313fb5aab
Heran76 <heran76@gmail.com>
2024-09-24 13:52:37 +0200

    leccion8

---
+++ anaya/punterosYreferencia/ejercicio8.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int edad, anio_actual, anio_nacimiento;
+    /*Solicitar la edad del usuario*/
+    printf("¿cual es tu edad? :");
+    scanf("%d",&edad);
+    fflush(stdin);
+
+    /*Solicitar el año acutal*/
+    printf("¿cual es el año actual?");
+    scanf("%d",&anio_actual);
+    fflush(stdin);
+
+    /*Calculamos el año nacimiento*/
+    anio_nacimiento= edad - anio_actual;
+    printf("Su año de nacimientos es %d", anio_nacimiento);
+    return 0;
+}

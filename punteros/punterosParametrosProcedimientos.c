a0830377376f67a296228409bb0ec4db29b2d30b
Heran76 <heran76@gmail.com>
2024-09-26 20:25:59 +0200

    punteroExtructura

--- punteros/punterosParametrosProcedimientos.c
+++ punteros/punterosParametrosProcedimientos.c
@@ -1,6 +1,6 @@
 /*
  Punteros como parámetros de entrada a procedimientos.
-*/
+
 #include <stdio.h>
 #include <stdio.h>
 
@@ -15,4 +15,21 @@ int main()
      procd1(&a); // para pasar el contenido del procedimiento hay que adignarele la memoria como parámetro
      printf("%d",a);
      return 0;
+}
+*/
+
+#include <stdio.h>
+
+int procd(int *a)
+{
+    *a = *a * 100;
+}
+
+
+int main()
+{
+  int a = 125;
+  procd(&a);
+  printf("%d ", a);
+  return 0;
 }
